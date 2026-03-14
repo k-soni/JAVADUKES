3dc4087cadd39666b8ae66473c2d59634a86dbf5
Kamal Soni <kamalsn022@gmail.com>
2026-03-14 12:50:15 +0530

    feat: search the element in matrix

--- src/main/java/com/ksoni/javadukes/medium/SearchMatrix.java
+++ src/main/java/com/ksoni/javadukes/medium/SearchMatrix.java
@@ -1,33 +1,37 @@
+package com.ksoni.javadukes.medium;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class test {
+public class SearchMatrix {
     public static void main(final String[] args) {
         ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
         list.add(new ArrayList<Integer>(Arrays.asList(1,2,3)));
         list.add(new ArrayList<Integer>(Arrays.asList(4,5,6)));
         list.add(new ArrayList<Integer>(Arrays.asList(7,8,9)));
+        System.out.println("list "+ list.size());
         test solution = new test();
-        solution.searchMatrix(list, 5);
+        int res = solution.searchMatrix(list, 5);
+        System.out.println("ans "+ res);
     }
 
     public int searchMatrix(ArrayList<ArrayList<Integer>> A, int B) {
         int ans = 0;
 
-        for (int i = 0; i < A.size()-1; i++) {
+        for (int i = 0; i < A.size(); i++) {
             int left = 0;
-            int right = A.get(i).size() - 1;
-            System.out.println("right "+ right);
+            ArrayList<Integer> subList = A.get(i);
+            int right = subList.size() - 1;
 
             while (left <= right) {
                 int mid = left + (right - left) / 2;
-
-                if(mid == B) {
+                int currentElement = subList.get(mid);
+                if(currentElement == B) {
                     ans = 1;
                     return ans;
                 }
 
-                if(mid > B) {
+                if(currentElement > B) {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
@@ -37,4 +41,6 @@ public class test {
 
         return ans;
     }
+
+
 }
