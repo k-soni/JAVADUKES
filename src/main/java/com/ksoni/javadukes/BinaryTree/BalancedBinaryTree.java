825ff74c74ffc17c8abd2cd694cc888ab0635bfa
Kamal Soni <kamalsn022@gmail.com>
2026-04-15 10:30:46 +0530

    feat: Balanced tree

---
+++ src/main/java/com/ksoni/javadukes/BinaryTree/BalancedBinaryTree.java
@@ -0,0 +1,12 @@
+package com.ksoni.javadukes.BinaryTree;
+
+import com.ksoni.javadukes.common.TreeNode;
+
+public class BalancedBinaryTree {
+    int isBalanced;
+
+    public static void main(String[] args) {
+
+    }
+
+}
