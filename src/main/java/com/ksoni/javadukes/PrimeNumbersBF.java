192d5734cae90dd72b5b7b9f704efb8fba8f329e
Kamal Soni <kamalsn022@gmail.com>
2026-04-27 11:58:48 +0530

    feat: Prime number brute force

---
+++ src/main/java/com/ksoni/javadukes/PrimeNumbersBF.java
@@ -0,0 +1,54 @@
+package com.ksoni.javadukes;
+
+import java.util.Arrays;
+
+public class PrimeNumbersBF {
+    public static void main(String[] args) {
+        PrimeNumbersBF primeNumbersBF = new PrimeNumbersBF();
+        primeNumbersBF.solve(14);
+    }
+
+    public int[] solve(int A) {
+        int[] output = new int[A+1];
+
+        output[0] = 0;
+        output[1] = 0;
+
+        for (int i = 1; i <=A ; i++) {
+            int factors = countFactors(i);
+            if(factors == 2) {
+                output[i] = i;
+            }
+        }
+
+
+        return Arrays.stream(output).filter(num -> num != 0).toArray();
+
+    }
+
+
+    public int countFactors(int A) {
+        if(A <= 0) {
+            return 0;
+        }
+
+        if(A == 1) {
+            return 1;
+        }
+
+        int count = 0;
+
+        for (int i = 1; i * i <=A ; i++) {
+            if(A%i == 0) {
+               if(i*i == A) {
+                   count++;
+               } else {
+                   count = count + 2;
+               }
+            }
+        }
+        return count;
+    }
+
+
+}
