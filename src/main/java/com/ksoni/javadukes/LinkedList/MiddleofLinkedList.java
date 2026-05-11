40c617e449a2c50a6c5ba25d08db5c135e63abaa
Kamal Soni <sonqkama@amazon.com>
2026-05-11 18:03:45 +0530

    feat: Middle of the linked list

---
+++ src/main/java/com/ksoni/javadukes/LinkedList/MiddleofLinkedList.java
@@ -0,0 +1,32 @@
+package com.ksoni.javadukes.LinkedList;
+
+import com.ksoni.javadukes.common.ListNode;
+
+public class MiddleofLinkedList {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode getMiddleNode(ListNode A) {
+        if(A == null) {
+            return A;
+        }
+
+        ListNode slowP = A;
+        ListNode fastP = A;
+
+
+        while (fastP.next != null && fastP.next.next != null) {
+            slowP = slowP.next;
+            fastP = fastP.next.next;
+        }
+
+        if(fastP.next == null) {
+            return slowP;
+        } else {
+            return slowP.next;
+        }
+
+    }
+
+}
